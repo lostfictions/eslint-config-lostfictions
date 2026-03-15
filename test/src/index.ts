6cce63b69de1411a8bad48cae04462ae6e29609c
lostfictions <lostfictions@users.noreply.github.com>
2026-03-14 22:44:08 -0400

    bump node ver, pnpm ver, initial deps

--- test/src/index.ts
+++ test/src/index.ts
@@ -41,9 +41,7 @@ const xy = `do you think ${/haha yeah/} won't stringify?`;
 const xx = `do you think ${new Error("whatever")} won't stringify?`;
 const xz = `do you think ${new URL("https://ok.com")} won't stringify?`;
 
-// we'd prefer if this passed, actually, but apparently it doesn't look up the prototype chain
 class MyError extends Error {}
-// eslint-disable-next-line @typescript-eslint/no-base-to-string, @typescript-eslint/restrict-template-expressions
 const xzz = `do you think ${new MyError("anotherthing")} won't stringify?`;
 
 // eslint-disable-next-line unicorn/prefer-string-slice, @typescript-eslint/no-deprecated
